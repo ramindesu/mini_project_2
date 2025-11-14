3d7de45b0a13388f84ce95647be7397ea1a5d2fa
ramin <ramindesu88@gmail.com>
2025-11-14 10:11:22 +0330

    ADD(base model)

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    delatable = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
