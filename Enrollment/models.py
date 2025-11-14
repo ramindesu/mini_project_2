a60236ab6ff566a2f3322a2f262dc5dfcbe5fd7e
ramin <ramindesu88@gmail.com>
2025-11-14 10:37:49 +0330

    ADD(Enroll )

--- Enrollment/models.py
+++ Enrollment/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from Accounts.models import Participants , BaseModel
+from Workshop.models import Workshop
 # Create your models here.
+
+
+
+class Enrollment(BaseModel):
+    Workshop = models.ForeignKey(Workshop , on_delete= models.CASCADE)
+    Participants = models.ForeignKey(Workshop , on_delete= models.CASCADE)
+    enroll_date = models.DateField()
+
+    def __str__(self):
+        return {self.Workshop.title}
+    
+    class Meta:
+        verbose_name = 'Enrollment'
+        verbose_name_plural = 'Enrollments'
+        ordering = ['enroll_date']
