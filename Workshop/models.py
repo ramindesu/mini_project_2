131d4405a3d8bbf4ac9e11e327e3704d4c57a739
ramin <ramindesu88@gmail.com>
2025-11-14 10:30:55 +0330

    ADD(workshop)

--- Workshop/models.py
+++ Workshop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from Accounts.models import BaseModel , Instructor
 
 # Create your models here.
+
+class Workshop(BaseModel):
+    title = models.CharField(max_length=60)
+    address = models.TextField()
+    teacher = models.ForeignKey(Instructor , on_delete=models.SET_NULL)
+    def __str__(self):
+        return f"{self.title} - Teacher: {self.teacher.name}"
+    
+    class Meta:
+        verbose_name = 'Workshop'
+        verbose_name_plural = 'Workshops'
+        ordering = ['name']
+        
